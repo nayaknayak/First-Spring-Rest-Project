bec7180230f67422f26527a2149f7d155e7a853d
satyabrat <nayaknayak12390@gmail.com>
2023-09-05 12:24:56 +0530

    First rest project

---
+++ src/main/java/com/sat/controller/WishMessageController.java
@@ -0,0 +1,34 @@
+package com.sat.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/message")
+public class WishMessageController {
+	
+	@GetMapping("/wish")
+	public ResponseEntity<String> getMessage(){
+		LocalDateTime time=LocalDateTime.now();
+		
+		String msg=null;
+		int hour=time.getHour();
+		if(hour<12) {
+			msg="Good Morning";
+		}else if(hour<16) {
+			msg="Good AfterNoon";
+		}else if(hour<20){
+			msg="Good Night";
+		}
+		ResponseEntity<String> entity=new ResponseEntity<String>(msg,HttpStatus.OK);
+		return entity;
+	}
+
+}
